3ca16a73cb082bdefeec08a4ac364dac16196276
zhidev <dzlij13@gmail.com>
2026-09-20 06:31:59 -0700

    Bit of overengineering. dex works tho!

--- src/components/rotomdex/RotomDex.jsx
+++ src/components/rotomdex/RotomDex.jsx
@@ -1,8 +1,9 @@
-import {useState} from 'react'
+import {useState, useRef} from 'react'
 import './RotomDex.css'
 import ImportPokemonForm from '../ImportPokemonForm.jsx';
 import rotomDexImage from '../../assets/rotomDex.png';
-
+import rotomClosedImage from '../../assets/pokeball.png';
+import rotomOpenedImage from '../../assets/powerbutton.png'
 
 function RotomDex({onSubmit, nodeCount}){
     const [rotomStatus, setRotomStatus] = useState(false);
@@ -10,12 +11,18 @@ function RotomDex({onSubmit, nodeCount}){
         x: 100,
         y: 50
     });
+    const didDrag = useRef(false)
 
     function startDrag(event){
           console.log("START DRAG");
-
+        didDrag.current=false;
+        //form controls what wont start dragging
+        if (event.target.closest('textarea, input, .importPokemonButton')) {
+            return;
+        }
         const startX = event.clientX
         const startY = event.clientY
+
 
         const startPosition = {
             x: position.x,
@@ -24,7 +31,15 @@ function RotomDex({onSubmit, nodeCount}){
 
 
         function drag(event) {
-                console.log("DRAGGING", event.clientX, event.clientY);
+            console.log("DRAGGING", event.clientX, event.clientY);
+
+            const moveX = event.clientX - startX;
+            const moveY = event.clientY - startY;
+
+             //If moved more than 5 units to prevent small mouse wigglies
+            if (Math.abs(moveX) > 5 || Math.abs(moveY) > 5) {
+                didDrag.current = true;
+            }
 
             setPosition({
                 x: startPosition.x + event.clientX - startX,
@@ -47,32 +62,46 @@ function RotomDex({onSubmit, nodeCount}){
 
     return(
         <>
-            <button
-                className="rotomDexToggleButton"
-                onClick={()=>setRotomStatus((current)=> !current)}
-            >
-                Toggle Rotom Dex TBC
-            </button>
-
             <div
-                 className={`rotomDex ${rotomStatus ? 'open' : 'hidden'}`}
-                 style={{
+                className="rotomDexWrapper"
+                style={{
                     left: position.x,
-                    top: position.y,
-                    backgroundImage: `url(${rotomDexImage})`
-                 }}
+                    top: position.y
+                }}
+            onPointerDown={startDrag}
             >
-                <div
-                    className="rotomDexDragHandle"
-                    onPointerDown={startDrag}
+                <button
+                    className="rotomDexToggleButton"
+                    onClick={()=> {
+                        if(didDrag.current) {
+                            didDrag.current = false;
+                            return;
+                        }
+                        setRotomStatus((current)=> !current)}
+                    }
                 >
-                    RotomDex
-                </div>
+                    <img
+                        src={rotomStatus ? rotomOpenedImage : rotomClosedImage}
+                        alt="Toggle RotomDex"
+                        draggable={false}
+                    />
+                </button>
 
-                <ImportPokemonForm
-                onSubmit={onSubmit}
-                nodeCount={nodeCount}
-                />
+                <div
+                    className={`rotomDex ${rotomStatus ? 'open' : 'hidden'}`}
+                >
+                    <img
+                        className="rotomDexImage"
+                        src={rotomDexImage}
+                        alt=""
+                        draggable={false}
+                    />
+
+                    <ImportPokemonForm
+                        onSubmit={onSubmit}
+                        nodeCount={nodeCount}
+                    />
+                </div>
             </div>
         </>
     );
